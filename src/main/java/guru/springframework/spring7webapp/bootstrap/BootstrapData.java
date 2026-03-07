9cbda08875977773e50a4b8a07ccacd91cb37d9f
janso <jansoh@ikmail.com>
2026-03-07 20:46:53 +0100

    2. Assignment 1: Hands On Assignment - Add Publisher

--- src/main/java/guru/springframework/spring7webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring7webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring7webapp.bootstrap;
 
 import guru.springframework.spring7webapp.domain.Author;
 import guru.springframework.spring7webapp.domain.Book;
+import guru.springframework.spring7webapp.domain.Publisher;
 import guru.springframework.spring7webapp.repositories.AuthorRepository;
 import guru.springframework.spring7webapp.repositories.BookRepository;
+import guru.springframework.spring7webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -49,8 +53,18 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
+        Publisher john = new Publisher();
+        john.setPublisherName("Jooohhn");
+        john.setAddress("Address of J");
+        john.setCity("City of J");
+        john.setState("State of J");
+        john.setZip("ZIP of J");
+
+        publisherRepository.save(john);
+
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Pub Count: " + publisherRepository.count());
     }
 }
